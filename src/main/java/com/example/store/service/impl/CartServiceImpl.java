05ebd9c64cd3799fb1ae63d731a3e296440ae02b
NongNghia07 <114387875+NongNghia07@users.noreply.github.com>
2024-10-20 12:44:31 +0700

    test push github

--- src/main/java/com/example/store/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/store/service/impl/CartServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.store.service.impl;
 
-public class CartServiceImpl {
+import com.example.store.dto.request.CartRequestDTO;
+import com.example.store.dto.response.util.ServiceResponseDTO;
+import com.example.store.repository.CartRepository;
+import com.example.store.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CartServiceImpl implements CartService {
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public CartServiceImpl(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+
+    @Override
+    public ServiceResponseDTO<?> create(CartRequestDTO cartRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponseDTO<?> update(CartRequestDTO cartRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponseDTO<?> delete(List<UUID> ids) {
+        return null;
+    }
 }
